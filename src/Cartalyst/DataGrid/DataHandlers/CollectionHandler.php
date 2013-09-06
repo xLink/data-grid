151f68b6295b2b241aace1c155fbcf2dad4784db
Tom Shafer <tj@tjshafer.com>
2013-09-06 14:26:26 -0700

    Switched to strnatcasecmp

--- src/Cartalyst/DataGrid/DataHandlers/CollectionHandler.php
+++ src/Cartalyst/DataGrid/DataHandlers/CollectionHandler.php
@@ -188,7 +188,7 @@ class CollectionHandler extends BaseHandler implements HandlerInterface {
 
 		$this->data = $this->data->sort(function($a, $b) use ($column, $direction)
 		{
-			$result = strnatcmp(strtolower($a[$column]), strtolower($b[$column]));
+			$result = strnatcasecmp($a[$column], $b[$column]);
 
 			$invert = ($direction == 'desc');
 
