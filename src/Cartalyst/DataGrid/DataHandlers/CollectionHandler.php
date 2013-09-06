a9089b81cf6b40aed931969486034520d4372ff6
Tom Shafer <tj@tjshafer.com>
2013-09-06 12:02:49 -0700

    Update for sorting when you have upper and lowercase.

--- src/Cartalyst/DataGrid/DataHandlers/CollectionHandler.php
+++ src/Cartalyst/DataGrid/DataHandlers/CollectionHandler.php
@@ -188,7 +188,7 @@ class CollectionHandler extends BaseHandler implements HandlerInterface {
 
 		$this->data = $this->data->sort(function($a, $b) use ($column, $direction)
 		{
-			$result = strnatcmp($a[$column], $b[$column]);
+			$result = strnatcmp(strtolower($a[$column]), strtolower($b[$column]));
 
 			$invert = ($direction == 'desc');
 
