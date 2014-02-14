bd4070902f49b2c430ea875856976ab2a4e2a5e6
Suhayb El Wardany <me@suw.me>
2014-02-14 20:05:45 +0200

    fix pagination bug
    Signed-off-by: Suhayb El Wardany <me@suw.me>

--- public/js/data-grid.js
+++ public/js/data-grid.js
@@ -13,7 +13,7 @@
  * @version    2.0.0
  * @author     Cartalyst LLC
  * @license    BSD License (3-clause)
- * @copyright  (c) 2011 - 2013, Cartalyst LLC
+ * @copyright  (c) 2011-2014, Cartalyst LLC
  * @link       http://cartalyst.com
  */
 
@@ -1065,7 +1065,7 @@
 
 					params = {
 						pageStart: perPage === 0 ? 0 : ( i === 1 ? 1 : (perPage * (i - 1) + 1)),
-						pageLimit: i === 1 ? perPage : ( pagi.totalCount < (perPage * i)) ? pagi.totalCount : perPage * 1,
+						pageLimit: i === 1 ? perPage : (pagi.totalCount < this.opt.throttle && i === this.opt.dividend) ? pagi.totalCount : perPage * i,
 						nextPage: next,
 						prevPage: prev,
 						page: i,
